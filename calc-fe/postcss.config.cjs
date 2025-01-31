03ccece5d95b798c4f6f07be0b9bdc6b407f85aa
Khehan Lakkitha <lakkithapeiris@gmail.com>
2025-01-31 18:05:54 +0530

    Added all the files

---
+++ calc-fe/postcss.config.cjs
@@ -0,0 +1,15 @@
+
+module.exports = {
+    plugins: {
+        'postcss-preset-mantine' : {},
+        'postcss-simple-vars' : {
+            Variables: {
+                'mantine-breakpoint-xs' : '36em',
+                'mantine-breakpoint-xs' : '48em',
+                'mantine-breakpoint-xs' : '62em',
+                'mantine-breakpoint-xs' : '75em',
+                'mantine-breakpoint-xs' : '88em',
+            },
+        },
+    },
+};
